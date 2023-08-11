311e13c30db1c011d2bd3d1b2076586a342e35dd
Callum <calrossmaclean@gmail.com>
2023-08-11 10:24:30 -0400

    More work on radio

--- scripts/radio.gd
+++ scripts/radio.gd
@@ -1,5 +1,11 @@
 extends Node3D
 
+var Songs:Dictionary = {
+	0 : ["None"]
+}
+
+var DisplayFrom:int = 0
+ # Use substr for string
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +15,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_timer_timeout():
+	pass # Replace with function body.
