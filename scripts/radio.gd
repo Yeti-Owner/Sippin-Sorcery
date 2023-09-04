553ad6b0728a30fefdf3ef5a34f814b422e22b09
Callum <calrossmaclean@gmail.com>
2023-09-03 22:04:41 -0400

    Working(?) Radio display
    Took a little break for college but I'm getting back in the rhythm of things. Not sure I can do daily commits again but I've definitely begun work again.

--- scripts/radio.gd
+++ scripts/radio.gd
@@ -1,20 +1,21 @@
 extends Node3D
 
+@onready var RadioDisplay = $Label3D
+
 var Songs:Dictionary = {
-	0 : ["None"]
+	0 : ["Sunsets - @itsskyebaby", "res://assets/audio/music/Sunsets.wav"],
+	1 : ["Vacation - @itsskyebaby", "res://assets/audio/music/Vacation.wav"]
 }
+var CurrentSong:int = 0
+var DisplayLength:int = 12
 
 var DisplayFrom:int = 0
- # Use substr for string
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+# when I wrote this code only God and I understood what it did
+# now only God knows
 func _on_timer_timeout():
-	pass # Replace with function body.
+	var NewSongDisplay := str(Songs[CurrentSong][0] + "        " + Songs[CurrentSong][0])
+	if NewSongDisplay.substr(DisplayFrom, DisplayLength) == NewSongDisplay.substr(0, DisplayLength):
+		DisplayFrom = 0
+	RadioDisplay.text = NewSongDisplay.substr(DisplayFrom, DisplayLength)
+	DisplayFrom += 1
