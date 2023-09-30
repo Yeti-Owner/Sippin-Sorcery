f47d7b18767e781f7425ab286418bb29f0ad3368
Callum <calrossmaclean@gmail.com>
2023-09-30 09:58:14 -0400

    Completed boss UI sys?
    I think it's complete, just need to make the physical bosses now ig

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -6,6 +6,9 @@ extends Node
 @onready var Game := $GameScene/Game
 
 @onready var BossName := $CanvasLayer/Boss/BossName
+@onready var BossBar := $CanvasLayer/Boss/BossBar
+signal BossDone(Success:bool)
+var BossTime := 0
 
 var SceneToLoad: String
 var CurrentScene:Node
@@ -16,6 +19,7 @@ var CurrentMouse := Input.MOUSE_MODE_VISIBLE
 func _ready():
 	Game.use_occlusion_culling = true
 	Engine.set_max_fps(120)
+	BossDone.connect(_end_boss)
 
 func _change_scene(scene:String, type:String = "normal"):
 	# Room for new transitions later
@@ -63,24 +67,35 @@ func _swap_hud(hud = null):
 
 func _on_game_scene_mouse_entered():
 	return
-	@warning_ignore("unreachable_code")
 	Input.set_mouse_mode(CurrentMouse)
 
 func _on_game_scene_mouse_exited():
 	return
-	@warning_ignore("unreachable_code")
 	CurrentMouse = Input.get_mouse_mode()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func _start_boss(Name:String, time:float):
+func _start_boss(Name:String, time:int = 100):
+	BossBar.max_value = 100
 	BossName.text = Name
 	TransitionPlayer.play("boss_in")
-	var tween = get_tree().create_tween()
-	tween.tween_property($CanvasLayer/Boss/BossBar, "value", 0, time)
-	tween.tween_callback(_end_boss(false))
+	BossTime = time
 
 func _end_boss(success:bool):
-	pass
+	if success:
+		var tween := get_tree().create_tween()
+		tween.tween_property(BossBar, "value", 0, 3)
+		tween.tween_callback(_end_boss.bind(false))
+	else:
+		TransitionPlayer.play("boss_out")
 
+func _on_timer_timeout():
+	BossBar.value = max(0, BossBar.value - 1)
+	if BossBar.value > 0:
+		$Timer.start()
+	else:
+		BossDone.emit(false)
 
-
+func _on_anim_player_animation_finished(anim_name):
+	if anim_name == "boss_in":
+		BossBar.max_value = BossTime
+		$Timer.start()
